ebdb2cdfa785a721d41a2f261c25e377b1040876
CoderDwang <coderdwang@outlook.com>
2019-02-02 20:19:48 +0800

    init

---
+++ Podfile
@@ -0,0 +1,10 @@
+target 'iMeiJu_Mac' do
+use_frameworks!
+
+    pod 'SwiftyJSON', '~> 4.2.0' # 转模型
+
+    pod 'SnapKit', '~> 4.2.0' # 自动布局
+
+    pod 'Moya', '~> 12.0.1' # 网络层
+
+end
---
+++ iMeiJu_Mac/AppDelegate.swift
@@ -0,0 +1,27 @@
+////  AppDelegate.swift
+//  iMeiJu_Mac
+//
+//  Created by iizvv on 2019/2/2.
+//	QQ群:	577506623
+//	GitHub:	https://github.com/iizvv
+//  Copyright © 2019 iizvv. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
