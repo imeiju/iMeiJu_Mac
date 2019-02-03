8d6d1561bef61e2c4ec7b11f52a30dc5eafab702
CoderDwang <coderdwang@outlook.com>
2019-02-03 22:12:09 +0800

    首页sectiontitle完成

--- iMeiJu_Mac/Contros/IZMainViewController.swift
+++ iMeiJu_Mac/Contros/IZMainViewController.swift
@@ -38,6 +38,8 @@ class IZMainViewController: NSViewController {
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(NSNib(nibNamed: "IZMainViewItem", bundle: nil), forItemWithIdentifier: NSUserInterfaceItemIdentifier(rawValue: "cell"))
+        
+        collectionView.register(NSNib(nibNamed:"IZMainSectionHeaderView", bundle: nil), forSupplementaryViewOfKind: NSCollectionView.elementKindSectionHeader, withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "header"))
     }
     
     func network() {
@@ -61,12 +63,13 @@ class IZMainViewController: NSViewController {
         layout.sectionInset = NSEdgeInsetsZero
         layout.minimumLineSpacing = CGFloat.leastNormalMagnitude
         layout.minimumInteritemSpacing = CGFloat.leastNormalMagnitude
+        layout.sectionHeadersPinToVisibleBounds = true
         return layout
     }
     
 }
 
-extension IZMainViewController: NSCollectionViewDataSource, NSCollectionViewDelegate {
+extension IZMainViewController: NSCollectionViewDataSource, NSCollectionViewDelegate, NSCollectionViewDelegateFlowLayout {
     func numberOfSections(in collectionView: NSCollectionView) -> Int {
         if model?.code==0 {
             print(model?.data.count as Any)
@@ -87,5 +90,14 @@ extension IZMainViewController: NSCollectionViewDataSource, NSCollectionViewDele
         return item
     }
     
+    func collectionView(_ collectionView: NSCollectionView, viewForSupplementaryElementOfKind kind: NSCollectionView.SupplementaryElementKind, at indexPath: IndexPath) -> NSView {
+        let view = collectionView.makeSupplementaryView(ofKind: kind, withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "header"), for: indexPath) as! IZMainSectionHeaderView
+        view.setSectionHeader(title: (model?.data[indexPath.section].name)!)
+        return view
+    }
+    
+    func collectionView(_ collectionView: NSCollectionView, layout collectionViewLayout: NSCollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> NSSize {
+        return NSSize(width: 1002, height: 40)
+    }
     
 }
---
+++ iMeiJu_Mac/Views/IZMainSectionHeaderView.swift
@@ -0,0 +1,26 @@
+////  IZMainSectionHeaderView.swift
+//  iMeiJu_Mac
+//
+//  Created by iizvv on 2019/2/3.
+//	QQ群:	577506623
+//	GitHub:	https://github.com/iizvv
+//  Copyright © 2019 iizvv. All rights reserved.
+//
+
+import Cocoa
+
+class IZMainSectionHeaderView: NSView {
+
+    @IBOutlet weak var sectionTitle: NSTextField!
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+        NSColor(calibratedWhite: 0.8 , alpha: 0.8).set()
+        __NSRectFillUsingOperation(dirtyRect, NSCompositingOperation.sourceOver)
+    }
+    
+    func setSectionHeader(title: String) {
+        sectionTitle.stringValue = title
+    }
+    
+}
